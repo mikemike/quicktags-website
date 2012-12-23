1708ef4b219916614fd2384608ffde4cdf61ac02
Mike Griffiths <mgriffiths@gmail.com>
2012-12-23 18:59:17 +0000

    Renamed .html to .php, started build of contact page

---
+++ contact.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Contact QuickTags, the NFC bluetooth toggle stickers</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Email QuickTags using the contact form. Request custom NFC tags">
+
+    <!-- Le styles -->
+    <link href="/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="/assets/css/styles.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-1749798-30']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
+  </head>
+
+  <body>
+
+    <div class="container-narrow">
+
+      <div class="row-fluid marketing">
+        <div class="span12">
+          <h1>Contact QuickTags</h1>
+          <p>
+            Got any questions for us?  Get in touch using the form below.
+          </p>
+
+          <h2>Custom Tags</h2>
+          <p>
+            We offer bespoke solutions for individuals or businesses for specific needs.
+            An example of this could be tags installed within all of the tables in a restaurant,
+            allowing customers to place their phone aganst the table to connect to the
+            restaurant WiFi, open a web page asking to sign up to a newsletter or even
+            download the menu to their device.
+          </p>
+          <p>
+            If you are interested in a quote for a custom solution please use the form
+            below.
+          </p>
+        </div>
+      </div>
+
+      <div class="row-fluid">
+        <div class="span12">
+          <h3>Contact Form</h3>
+          <form class="form-horizontal">
+            <div class="control-group">
+              <label class="control-label" for="inputName">Name</label>
+              <div class="controls">
+                <input type="text" id="inputName" placeholder="Name" name="name">
+              </div>
+            </div>
+            <div class="control-group">
+              <label class="control-label" for="inputEmail">Email</label>
+              <div class="controls">
+                <input type="email" id="inputEmail" placeholder="Email" name="email">
+              </div>
+            </div>
+            <div class="control-group">
+              <label class="control-label" for="inputPhone">Phone</label>
+              <div class="controls">
+                <input type="tel" id="inputPhone" placeholder="Phone" name="tel">
+              </div>
+            </div>
+            <div class="control-group">
+              <label class="control-label" for="inputEnquiry">Enquiry</label>
+              <div class="controls">
+                <textarea id="inputEnquiry" name="enquiry"></textarea>
+              </div>
+            </div>
+            <div class="control-group">
+              <div class="controls">
+                <button type="submit" class="btn">Send</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <hr>
+
+      <div class="footer">
+        <p>&copy; QuickTags 2012</p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+  </body>
+</html>
